f69a3c7530bf7c746edc60cc720c4dc410e6c033
Luis Ricardo Sanchez <luissanchez0305@gmail.com>
2014-04-01 10:26:18 -0500

    ejemplo 2

--- js/index.js
+++ js/index.js
@@ -48,6 +48,25 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+$('#reposDetail').live('pageshow', function(event) {
+    var owner = getUrlVars().owner;
+    var name = getUrlVars().name;
+    loadRepoDetail(owner,name);
+});
+
+function loadRepoDetail(owner,name) {
+    $.ajax("https://api.github.com/repos/" + owner + "/" + name).done(function(data) {
+        var repo = data;
+        console.log(data);
+
+        $('#repoName').html("<a href='" + repo.homepage + "'>" + repo.name + "</a>");
+        $('#description').text(repo.description);
+        $('#forks').html("<strong>Forks:</strong> " + repo.forks + "<br><strong>Watchers:</strong> " + repo.watchers);
+
+        $('#avatar').attr('src', repo.owner.avatar_url);
+        $('#ownerName').html("<strong>Owner:</strong> <a href='" + repo.owner.url + "'>" + repo.owner.login + "</a>");
+    });
+}
 
 $('#reposHome').bind('pageinit', function(event) {
 	loadRepos();
@@ -57,10 +76,22 @@ function loadRepos() {
     $.ajax("https://api.github.com/legacy/repos/search/javascript").done(function(data) {
         var i, repo;
         $.each(data.repositories, function (i, repo) {
-            $("#allRepos").append("<li><a href='https://github.com/" + repo.username + "/" + repo.name + "'>"
+            $("#allRepos").append("<li><a href='repo-detail.html?owner=" + repo.username + "&name=" + repo.name + "'>"
             + "<h4>" + repo.name + "</h4>"
             + "<p>" + repo.username + "</p></a></li>");
         });
         $('#allRepos').listview('refresh');
     });
+}
+
+function getUrlVars() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
 }
