ce49bd01b4dc30582998c95a5fd4d8f4ec893373
Luis Ricardo Sanchez <luissanchez0305@gmail.com>
2014-04-01 19:00:37 -0500

    cambio menor para device id

--- js/index.js
+++ js/index.js
@@ -99,7 +99,7 @@ var db;
 $('#reposHome').bind('pageinit', function(event) {
     loadRepos();
     //db = window.openDatabase("repodb","0.1","GitHub Repo Db", 1024*1024*5);
-	alert(device.name);
+	//alert(device.name);
     //db.transaction(createDb, txError, txSuccess);
 });
 
