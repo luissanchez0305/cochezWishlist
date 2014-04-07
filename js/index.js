004a0a94f3aaeec4304a62ea4cb98e258b30ec11
Luis Ricardo Sanchez <luissanchez0305@gmail.com>
2014-04-06 19:35:31 -0500

    arreglo de interfase 2

--- js/index.js
+++ js/index.js
@@ -41,7 +41,6 @@ var app = {
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var value = window.localStorage.getItem("cochezwl_user");
-		alert(value);
         if(value)
         	if(value.length > 0){
         		changePage('list-page');
