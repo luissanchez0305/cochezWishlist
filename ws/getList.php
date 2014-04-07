4a4c386c2aeba35cc75dd3b26672b0284b740010
Luis Ricardo Sanchez <luissanchez0305@gmail.com>
2014-04-06 23:56:13 -0500

    agregar nuevo query

---
+++ ws/getList.php
@@ -0,0 +1,29 @@
+<?php
+if(isset($_GET['u'])) {
+	$username = 'espheras_dbuser';
+	$password = 'Goingupinlife123';
+	$database = 'cochezwl';
+
+	$link = mysql_connect('localhost',$username,$password) or die('Cannot connect to the DB');
+	mysql_select_db($database) or die(mysql_errno());
+	header('Content-type: application/json');
+	header("access-control-allow-origin: *");
+
+	/* grab the posts from the db */
+	$uname = $_GET['u'];
+	$pwd = $_GET['p'];
+	$query = "SELECT barcode, name FROM lists l JOIN barcodes b ON b.id = l.barcodeid WHERE userid = (SELECT id FROM users WHERE username = '$uname')";
+	$result = mysql_query($query,$link) or die('Errant query:  '.$query);
+
+	/* create one master array of the records */
+	$posts = array();
+	if(mysql_num_rows($result)) {
+		while($post = mysql_fetch_assoc($result)) {
+			$posts[] = array('post'=>$post);
+		}
+	}
+	echo json_encode(array('posts'=>$posts));
+
+	/* disconnect from the db */
+	@mysql_close($link);
+}
