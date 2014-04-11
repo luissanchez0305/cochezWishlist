827863e1b858bd21e60a0a968eab8c4c7791b4f9
Luis Ricardo Sanchez <luissanchez0305@gmail.com>
2014-04-11 12:32:44 -0500

    listado

--- ws/createitem.php
+++ ws/createitem.php
@@ -10,11 +10,26 @@ if(isset($_GET['u']) && isset($_GET['b'])) {
 	header('Content-type: application/json');
 	header("access-control-allow-origin: *");
 
-	$uname = $_GET['u'];
-	$bcode = $_GET['b'];
-	$query = "INSERT INTO lists (barcode, userid) VALUES ('$bcode', (SELECT id FROM users WHERE username = '$uname'))";
-	mysql_query($query,$link) or die('Errant query:  '.$query);
-	echo json_encode(mysql_insert_id());
+	$query = "SELECT id FROM barcodes WHERE barcode LIKE '$bcode'";
+	$result = mysql_query($query,$link) or die('Errant query:  '.$query);
+
+	/* create one master array of the records */
+	$posts = array();
+	if(mysql_num_rows($result)) {
+		while($post = mysql_fetch_assoc($result)) {
+			$posts[] = array('post'=>$post);
+		}
+	}
+	
+	if (count($posts) > 0) {
+		$uname = $_GEt['u'];
+		$bcode = $_GET['b'];
+		$query = "INSERT INTO lists (barcodeid, userid) VALUES ($posts[0].id, (SELECT id FROM users WHERE username = '$uname'))";
+		mysql_query($query,$link) or die('Errant query:  '.$query);
+		echo json_encode(mysql_insert_id());
+	}
+	else 
+		echo '0';
 
 	/* disconnect from the db */
 	@mysql_close($link);
